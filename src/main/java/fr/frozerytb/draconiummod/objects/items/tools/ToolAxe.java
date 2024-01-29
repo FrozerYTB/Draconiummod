96785b8e91be38c36b90d722a6ab2f1d3007e64d
FrozerYTB <frozerytb3@gmail.com>
2024-01-29 18:10:00 +0100

    toolsAxe
    ToolsAxe

--- src/main/java/fr/frozerytb/draconiummod/objects/items/tools/ToolAxe.java
+++ src/main/java/fr/frozerytb/draconiummod/objects/items/tools/ToolAxe.java
@@ -1,24 +1,42 @@
 package fr.frozerytb.draconiummod.objects.items.tools;
 
-import fr.frozerytb.draconiummod.Main;
+
+import java.util.Set;
+import com.google.common.collect.Sets;
 import fr.frozerytb.draconiummod.init.ItemInit;
 import fr.frozerytb.draconiummod.util.interfaces.IHasmodel;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemAxe;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
 
-public class ToolAxe extends ItemAxe implements IHasmodel {
-    public ToolAxe(String name, Item.ToolMaterial material)
+public class ToolAxe extends ItemTool implements IHasmodel
+{
+    private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE);
+
+    public ToolAxe(String name, ToolMaterial material)
     {
-        super(material);
+        super(material, EFFECTIVE_ON);
         setUnlocalizedName(name);
         setRegistryName(name);
-        setCreativeTab(CreativeTabs.MATERIALS);
+        setCreativeTab(CreativeTabs.TOOLS);
+
         ItemInit.ITEMS.add(this);
     }
+
+    public float getDestroySpeed(ItemStack stack, IBlockState state)
+    {
+        Material material = state.getMaterial();
+        return material != Material.WOOD && material != Material.PLANTS && material != Material.VINE ? super.getDestroySpeed(stack, state) : this.efficiency;
+    }
+
     @Override
-    public void registerModels() {
-        Main.proxy.registerItemRenderer(this, 0);
+    public void registerModels()
+    {
+
     }
 }
