6568983c9038c2a5c52e3611ec26e02dbfb2acdb
FrozerYTB <frozerytb3@gmail.com>
2024-04-09 20:41:43 +0200

    y
    y

--- src/main/java/fr/frozerytb/draconiummod/init/FluidInit.java
+++ src/main/java/fr/frozerytb/draconiummod/init/FluidInit.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fluids.FluidRegistry;
 
 public class FluidInit
 {
-    public static final Fluid FAKE_WATER_FLUID = new FluidFakeWater("fake_water", new ResourceLocation(Reference.MODID + ":blocks/white_water_still"), new ResourceLocation(Reference.MODID + ":blocks/white_water_flow"), new ResourceLocation(Reference.MODID + ":blocks/white_water_overlay"));
+    public static final Fluid FAKE_WATER_FLUID = new FluidFakeWater("fake_water", new ResourceLocation(Reference.MODID + ":blocks/fake_water_still"), new ResourceLocation(Reference.MODID + ":blocks/fake_water_flow"), new ResourceLocation(Reference.MODID + ":blocks/white_water_overlay"));
 
     public static void registerFluids()
     {
--- src/main/java/fr/frozerytb/draconiummod/objects/blocks/fluids/FluidFakeWater.java
+++ src/main/java/fr/frozerytb/draconiummod/objects/blocks/fluids/FluidFakeWater.java
@@ -1,7 +1,11 @@
 package fr.frozerytb.draconiummod.objects.blocks.fluids;
 
+
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
+
+import javax.swing.text.html.parser.Entity;
 
 public class FluidFakeWater extends Fluid
 {
@@ -9,5 +13,11 @@ public class FluidFakeWater extends Fluid
     {
         super(name, still, flow, overlay);
         this.setUnlocalizedName(name);
+        this.rarity(Enchantment.Rarity.RARE);
+
+    }
+
+
+    private void rarity(Enchantment.Rarity rarity) {
     }
 }
