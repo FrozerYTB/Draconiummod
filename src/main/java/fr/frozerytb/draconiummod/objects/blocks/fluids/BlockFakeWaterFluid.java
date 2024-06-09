7599a3d8b5dd75c979d80f4dc0f47a0a52655e00
FrozerYTB <frozerytb3@gmail.com>
2024-06-09 21:39:39 +0200

    y
    y

--- src/main/java/fr/frozerytb/draconiummod/objects/blocks/fluids/BlockFakeWaterFluid.java
+++ src/main/java/fr/frozerytb/draconiummod/objects/blocks/fluids/BlockFakeWaterFluid.java
@@ -1,19 +1,24 @@
 package fr.frozerytb.draconiummod.objects.blocks.fluids;
 
-import fr.frozerytb.draconiummod.Main;
 import fr.frozerytb.draconiummod.init.BlockInit;
 import fr.frozerytb.draconiummod.init.ItemInit;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 
-public class BlockFakeWaterFluid extends BlockFluidClassic{
-    public BlockFakeWaterFluid(String name, Fluid fluid, Material material)
-    {
+public class BlockFakeWaterFluid extends BlockFluidClassic {
+    public BlockFakeWaterFluid(String name, Fluid fluid, Material material) {
         super(fluid, material);
         this.setUnlocalizedName(name);
         this.setRegistryName(name);
@@ -22,9 +27,12 @@ public class BlockFakeWaterFluid extends BlockFluidClassic{
     }
 
     @Override
-    public EnumBlockRenderType getRenderType(IBlockState state)
-    {
-        return EnumBlockRenderType.MODEL;
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        if ((worldIn.isRemote || entityIn instanceof EntityItem || entityIn instanceof EntityBoat) ||
+                (entityIn instanceof EntityPlayer && (((EntityPlayer) entityIn).capabilities.isCreativeMode || (entityIn.getRidingEntity() instanceof EntityBoat)))) {
+            return;
+        }
+        entityIn.attackEntityFrom(DamageSource.DROWN, 3F);
     }
 }
 
