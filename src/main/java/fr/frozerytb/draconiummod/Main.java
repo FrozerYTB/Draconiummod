039b5ad918f7c2c1c6cd29772588716ec07197b2
FrozerYTB <frozerytb3@gmail.com>
2024-06-18 18:44:53 +0200

    y
    y

--- src/main/java/fr/frozerytb/draconiummod/Main.java
+++ src/main/java/fr/frozerytb/draconiummod/Main.java
@@ -3,8 +3,13 @@ package fr.frozerytb.draconiummod;
 import fr.frozerytb.draconiummod.proxy.CommonProxy;
 import fr.frozerytb.draconiummod.tabs.DraconiummodTab;
 import fr.frozerytb.draconiummod.util.Reference;
+import fr.frozerytb.draconiummod.util.handlers.PlayerJoinEventHandler;
 import fr.frozerytb.draconiummod.util.handlers.RegistryHandler;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -12,12 +17,14 @@ import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class Main {
+
     @Instance
-    public static Main Instance;
+    public static Main instance;
 
     @SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
     public static CommonProxy proxy;
@@ -25,20 +32,39 @@ public class Main {
 
     public static final CreativeTabs DraconiummodTab = new DraconiummodTab("draconiummod");
 
-    static
-    {
+    static {
         FluidRegistry.enableUniversalBucket();
     }
 
     @EventHandler
-    public static void preinit(FMLPreInitializationEvent e) {
-        logger = e.getModLog();
+    public void preinit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(SoundEventHandler.class);
+        MinecraftForge.EVENT_BUS.register(PlayerJoinEventHandler.class);
+        logger = event.getModLog();
 
         proxy.preInit();
         RegistryHandler.preInitRegistries();
     }
 
 
+
     @EventHandler
-    public static void init(FMLInitializationEvent e) {RegistryHandler.initRegistries();}
+    public void init(FMLInitializationEvent event) {
+        RegistryHandler.initRegistries();
+    }
+    
+    @Mod.EventBusSubscriber(modid = Reference.MODID)
+    public static class SoundEventHandler {
+
+        @SubscribeEvent
+        public static void registerSounds(RegistryEvent.Register<SoundEvent> event) {
+            final SoundEvent music = createSoundEvent("music.background");
+            event.getRegistry().register(music);
+        }
+
+        private static SoundEvent createSoundEvent(String soundName) {
+            final ResourceLocation soundID = new ResourceLocation(Reference.MODID, soundName);
+            return new SoundEvent(soundID).setRegistryName(soundID);
+        }
+    }
 }
