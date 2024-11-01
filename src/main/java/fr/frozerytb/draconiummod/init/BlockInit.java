c21cd0177ed18b3c2b0df9177b7bd8e4fe052a09
FrozerYTB <frozerytb3@gmail.com>
2024-11-01 16:13:02 +0100

    Merge branch 'master' of D:\Draconiummod with conflicts.

--- src/main/java/fr/frozerytb/draconiummod/init/BlockInit.java
+++ src/main/java/fr/frozerytb/draconiummod/init/BlockInit.java
@@ -1,23 +1,28 @@
 package fr.frozerytb.draconiummod.init;
 
-import fr.frozerytb.draconiummod.Main;
-import fr.frozerytb.draconiummod.objects.blocks.fluids.BlockFakeWaterFluid;
+import fr.frozerytb.draconiummod.objects.blocks.*;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+<<<<<<< HEAD
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
+=======
+>>>>>>> origin/master
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BlockInit {
+public class BlockInit
+{
 
-    public static final List<Block> BLOCKS = new ArrayList<>();
+    public static final List<Block> BLOCKS = new ArrayList<Block>();
 
-    // Déclaration des blocs
-    public static final Block FAKE_WATER_BLOCK = new BlockFakeWaterFluid("fake_water", FluidInit.FAKE_WATER_FLUID, Material.WATER);
+    public static final Block AZURITE_BLOCK = new BlockBasic("azurite_block", Material.IRON);
+    public static final Block DRACONIUM_BLOCK = new BlockBasic("draconium_block", Material.IRON);
 
+<<<<<<< HEAD
     public static void init() {
         // Assurez-vous que les fluides sont enregistrés d'abord
         FluidInit.initFluids();
@@ -34,14 +39,30 @@ public class BlockInit {
 
         registerBlocks();
     }
+=======
+    //BLOCS DE MINERAIS
+    public static final Block AZURITE_ORE = new BlockBasic("azurite_ore", Material.ROCK);
+    public static final Block DRACONIUM_ORE = new BlockBasic("draconium_ore", Material.ROCK);
+    public static final Block FINDIUM_ORE = new BlockBasic("findium_ore", Material.ROCK);
+    public static final Block EXPLOSIVE_ORE = new BlockExplosiveOre("explosive_ore", Material.ROCK);
+    public static final Block RANDOM_ORE = new BlockRandomOre("random_ore", Material.ROCK);
 
-    private static void registerBlocks() {
-        IForgeRegistry<Block> registry = GameRegistry.findRegistry(Block.class);
-        for (Block block : BLOCKS) {
-            block.setRegistryName(block.getUnlocalizedName().substring(5));
-            block.setCreativeTab(Main.DRACONIUMMOD_TAB);
-            registry.register(block);
-            System.out.println("Registered block: " + block.getRegistryName());
-        }
-    }
+    // Blocs spéciaux
+    public static final Block CAVE_BLOCK = new BlockCaveBlock("cave_block", Material.GLASS);
+    public static final Block ELEVATOR = new BlockElevator("elevator", Material.IRON);
+
+
+>>>>>>> origin/master
+
+
+
+
+
+
+
+
+
+
+
+
 }
